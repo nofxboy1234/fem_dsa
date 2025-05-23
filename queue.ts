d48a7fb63d42d0b073ea9306caeef270688689b0
Dylan Palmboom <nofxboy1234@gmail.com>
2025-05-23 16:57:36 +0200

    Add queue

---
+++ queue.ts
@@ -0,0 +1,64 @@
+type Node<T> = {
+  value: T;
+  next?: Node<T>;
+};
+
+export default class Queue<T> {
+  public length: number;
+  private head?: Node<T>;
+  private tail?: Node<T>;
+
+  constructor() {
+    this.head = this.tail = undefined;
+    this.length = 0;
+  }
+
+  enqueue(item: T): void {
+    const node = { value: item } as Node<T>;
+    this.length++;
+    if (!this.tail && !this.head) {
+      this.tail = this.head = node;
+      return;
+    }
+
+    this.tail!.next = node;
+    this.tail = node;
+  }
+
+  deque(): T | undefined {
+    if (!this.head) {
+      return undefined;
+    }
+
+    this.length--;
+
+    const head = this.head;
+    this.head = this.head.next;
+
+    head.next = undefined;
+
+    if (this.length === 0) {
+      this.tail = undefined;
+    }
+
+    return head.value;
+  }
+
+  peek(): T | undefined {
+    return this.head?.value;
+  }
+}
+
+const numberQueue = new Queue<number>();
+
+numberQueue.enqueue(10);
+numberQueue.enqueue(20);
+numberQueue.enqueue(30);
+
+console.log("Queue length:", numberQueue.length);
+console.log("First item (peek):", numberQueue.peek());
+
+console.log("Dequeued:", numberQueue.deque());
+console.log("Dequeued:", numberQueue.deque());
+console.log("Queue length:", numberQueue.length);
+console.log("Next item:", numberQueue.peek());
