0d82be73bd3e88dee788e1b2c16d56bed89edec7
Dylan Palmboom <nofxboy1234@gmail.com>
2025-05-25 12:41:10 +0200

    Add recursion

---
+++ recursion.ts
@@ -0,0 +1,17 @@
+export default function foo(n: number): number {
+  // Base Case
+  if (n === 1) {
+    console.log(`post: ${n}`);
+    return 1;
+  }
+
+  // Recurse!
+  // pre: n +
+  console.log(`pre: ${n}+`);
+  const out = n + foo(n - 1);
+  console.log(`post: ${n}`);
+  return out;
+}
+
+let result = foo(5);
+console.log(result);
