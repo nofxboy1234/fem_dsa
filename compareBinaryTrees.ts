2fbc6a9414cbc403c3a8ad4f2a9114fb29e31b12
Dylan Palmboom <nofxboy1234@gmail.com>
2025-05-27 13:35:41 +0200

    Add compareBinaryTrees

---
+++ compareBinaryTrees.ts
@@ -0,0 +1,23 @@
+import type { BinaryNode } from "./binaryNode";
+
+export default function compare(
+  a: BinaryNode<number> | null,
+  b: BinaryNode<number> | null
+): boolean {
+  // a and b are structurally the same
+  if (a === null && b === null) {
+    return true;
+  }
+
+  // a and b are structurally NOT the same
+  if (a === null || b === null) {
+    return false;
+  }
+
+  // value check
+  if (a.value !== b.value) {
+    return false;
+  }
+
+  return compare(a.left, b.left) && compare(a.right, b.right);
+}
