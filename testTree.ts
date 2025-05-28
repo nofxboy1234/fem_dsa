861196f8685884d346e5e1ed0adbfe60b5186a98
Dylan Palmboom <nofxboy1234@gmail.com>
2025-05-28 21:22:21 +0200

    Add testTree

---
+++ testTree.ts
@@ -0,0 +1,61 @@
+const testTree = {
+  value: 41,
+  left: {
+    value: 16,
+    left: {
+      value: 10,
+      left: {
+        value: 4,
+        left: null,
+        right: null,
+      },
+      right: null,
+    },
+    right: {
+      value: 27,
+      left: null,
+      right: {
+        value: 35,
+        left: null,
+        right: null,
+      },
+    },
+  },
+  right: {
+    value: 65,
+    left: {
+      value: 46,
+      left: {
+        value: 44,
+        left: null,
+        right: {
+          value: 45,
+          left: null,
+          right: null,
+        },
+      },
+      right: {
+        value: 53,
+        left: {
+          value: 52,
+          left: null,
+          right: null,
+        },
+        right: null,
+      },
+    },
+    right: {
+      value: 68,
+      left: {
+        value: 67,
+        left: {
+          value: 66,
+          left: null,
+          right: null,
+        },
+        right: null,
+      },
+      right: null,
+    },
+  },
+};
